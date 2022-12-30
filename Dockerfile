6d668c5b5ccdc72ff3a931184964a646decb76a2
Tobi <tobilg@gmail.com>
2022-12-30 13:41:12 +0000

    Update build logic & releases for Darwin and Linux for v2.6.2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM amazonlinux:2
 
+ARG CADDY_VERSION=v2.6.2
+ARG OS=linux
+
 # Install dependencies
 RUN yum update -y && \
   yum install golang -y
@@ -7,4 +10,4 @@ RUN yum update -y && \
 RUN GOBIN=/usr/local/bin/ go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 
 RUN mkdir -p /tmp/caddy-build && \
-  xcaddy build --with github.com/silinternational/certmagic-storage-dynamodb/v3 --output /tmp/caddy-build
+  GOOS=${OS} xcaddy build ${CADDY_VERSION} --with github.com/ss098/certmagic-s3 --with github.com/silinternational/certmagic-storage-dynamodb/v3 --output /tmp/caddy-build/aws_caddy_${CADDY_VERSION}_${OS}
--- build.sh
+++ build.sh
@@ -2,14 +2,23 @@
 
 set -e
 
-# Cleanup
-rm -rf $PWD/release
+# Set OS (first script argument)
+OS=${1:-linux}
 
-# Create release folder
-mkdir -p $PWD/release
+# Set Caddy version (second script argument)
+CADDY_VERSION=${2:-v2.6.2}
+
+# Create release folders
+mkdir -p $PWD/releases $PWD/temp_release
 
 # Run build
-docker build -t custom-caddy-build .
+docker build --build-arg OS=$OS --build-arg CADDY_VERSION=$CADDY_VERSION -t custom-caddy-build .
 
-# Copy release
-docker run -v $PWD/release:/opt/mount --rm -ti custom-caddy-build bash -c "cp /tmp/caddy-build/caddy /opt/mount/"
+# Copy release from image to temporary folder
+docker run -v $PWD/temp_release:/opt/mount --rm -ti custom-caddy-build bash -c "cp /tmp/caddy-build/* /opt/mount/"
+
+# Copy release to releases
+cp $PWD/temp_release/* $PWD/releases/
+
+# Cleanup
+rm -rf $PWD/temp_release
